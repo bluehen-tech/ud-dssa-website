8d01cbbe8b5a439c0b10a6d69182c033e4f10a0c
flyaflya <ajf@udel.edu>
2025-09-10 16:17:24 -0400

    clean up files

--- src/data/index.ts
+++
@@ -1,9 +0,0 @@
-// Export all data types and collections
-export * from './team';
-export * from './events';
-export * from './services';
-
-// Re-export types for convenience
-export type { TeamMember } from './team';
-export type { Event } from './events';
-export type { Service } from './services';
